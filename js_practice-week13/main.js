fe068f64fed55374f2f377ab80852b96d1b2de9d
nainanarain <72845323+nainanarain@users.noreply.github.com>
2024-04-28 16:14:23 -0700

    Create main.js

---
+++ js_practice-week13/main.js
@@ -0,0 +1,25 @@
+// declare a variable using the keyword "let" and the identifier aka the name of "cats" and assign the value of 22
+let cats = 22;
+
+// declare a variable using the identifier aka the name of "dogs" and assign the value of 13
+let dogs = 13;
+
+// declare a variable using the identifier aka the name of "addingVariables" and assign the expression of "cats + dogs"
+let addingVariables = cats + dogs;
+
+// declare a variable using the identifier aka the name of "addingStrings" and concat 'javascript is ' + 'really awesome!'
+let addingStrings = "javascript is ";
+addingStrings + "really awesome!";
+//another option for concatinating strings
+let addingStringsTwo = "javascript is ";
+let newString = "really awesome!";
+let sentence = addingStringsTwo + newString;
+
+// declare an array using the identifier aka the name of "myArray" and add these three items into it: 'Coding is awesome', 500, false
+myArray = ["Coding is awesome", 500, false];
+
+// resassign myArray[1] to 250
+myArray[1] = 250;
+
+// create your own variable and assign a random string/message to it
+let newVar = "hello world";
